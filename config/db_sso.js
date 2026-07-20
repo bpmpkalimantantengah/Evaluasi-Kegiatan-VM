5a41d164c69f74d90326df97343f185eef71290b
riza sukmawan <lpmp.kalimantantengah@gmail.com>
2026-07-21 06:42:44 +0700

    Deploy: Clean-Up & Optimasi GASPOL V2

--- config/db_sso.js
+++ config/db_sso.js
@@ -7,10 +7,11 @@ const dbSSO = mysql.createPool({
   password: process.env.DB_SSO_PASSWORD || process.env.DB_EVALUASI_PASSWORD || '',
   database: process.env.DB_SSO_NAME || 'gaspol_portal',
   waitForConnections: true,
-  connectionLimit: 15,
+  connectionLimit: 100,
   queueLimit: 0,
   dateStrings: true,
   timezone: '+08:00',
 });
 
 module.exports = dbSSO;
+
